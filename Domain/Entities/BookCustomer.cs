64f90f2bdf8d6ce6a0360402ddc0056ccae70c1b
Osama Musa <omusa@asaltech.com>
2021-11-17 11:17:18 +0200

    Edit book reserve

--- Domain/Entities/BookCustomer.cs
+++ Domain/Entities/BookCustomer.cs
@@ -6,14 +6,18 @@ using System.Text;
 
 namespace Domain.Entities
 {
-    class BookMember
+    class BookCustomer
     {
         [Key, Column(Order = 1)]
         public int BookId { get; set; }
+
         [Key, Column(Order = 2)]
         public int MemberId { get; set; }
         public Book Book { get; set; }
-        public BookMember Member { get; set; }
-        public DateTime Time { get; set; }
+        public Customer Customer { get; set; }
+        public DateTime reserveTime { get; set; }
+        public bool isReturned { get; set; }
+        public DateTime returnedTime { get; set; }
+
     }
 }
