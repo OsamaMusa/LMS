bd67e2a36f5704a79b81b3d748397f4abbde7964
Osama Musa <omusa@asaltech.com>
2021-11-17 11:24:28 +0200

    reserve book

--- Domain/Entities/BookCustomer.cs
+++ Domain/Entities/BookCustomer.cs
@@ -8,11 +8,11 @@ namespace Domain.Entities
 {
     class BookCustomer
     {
-        [Key, Column(Order = 1)]
+        [ForeignKey("Book")]
         public int BookId { get; set; }
 
-        [Key, Column(Order = 2)]
-        public int MemberId { get; set; }
+        [ForeignKey("Customer")]
+        public int CustomerId { get; set; }
         public Book Book { get; set; }
         public Customer Customer { get; set; }
         public DateTime reserveTime { get; set; }
